cfef7bd3b0963506c481074751e25c4639a08d1c
mansionis-dev <54140090+mansionis-dev@users.noreply.github.com>
2020-04-03 09:49:56 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,4 +7,8 @@ ssh -V
 cat /etc/os-release
 uname -a
 echo ''
+git clone https://github.com/magento/magento2.git /var/www/magento2
+cd /var/www/magento2
+composer install --optimize-autoloader --no-interaction --no-progress
+
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
